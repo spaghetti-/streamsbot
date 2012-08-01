d6707852cd5bfa9ad5ddc542f47744f98d35ec80
potatoe <ncmpcd@gmail.com>
2012-08-01 11:44:14 +0800

    Added bot framework.

--- streams.py
+++ streams.py
@@ -12,7 +12,7 @@ import unicodedata
 #dev-python/twisted-words
 
 from twisted.words.protocols import irc
-from twisted.internet import protocol
+from twisted.internet import protocol, reactor
 
 import sqlite3 as sqlite
 
@@ -168,8 +168,45 @@ class Streams:
 		row = self.cur.fetchall()
 		return len(row)
 
-temp = Streams()
+
+class StreamsBot(irc.IRCClient):
+    def _get_nickname(self):
+        return self.factory.nickname
+    nickname = property(_get_nickname)
+
+    def signedOn(self):
+        self.join(self.factory.channel)
+        print "Signed on as %s." % (self.nickname)
+
+    def joined(self, channel):
+        print "Joined %s." % (channel,)
+
+    def privmsg(self, user, channel, msg):
+        print msg
+
+class StreamsBotFactory(protocol.ClientFactory):
+    protocol = StreamsBot
+
+    def __init__(self, channel, nickname='RSB'):
+        self.channel = channel
+        self.nickname = nickname
+
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection (%s), reconnecting." % (reason,)
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Could not connect: %s" % (reason,)
+
+
+#temp = Streams()
 #temp.update_streams()
-print temp.get_count()
+#print temp.get_count()
 #temp.truncateTable()
 #temp.import_list('streams.list')
+
+if __name__ == '__main__':
+	#f = StreamsBotFactory('#test')
+	#reactor.connectTCP('irc.quakenet.org', 6667, f)
+    reactor.connectTCP('irc.quakenet.org', 6667, StreamsBotFactory('#test'))
+    reactor.run()
