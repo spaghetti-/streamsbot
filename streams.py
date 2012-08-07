9cc395380772ca004ef20f5a8e68d5a1d826a6a3
potatoe <dev@xrng.net>
2012-08-07 14:08:38 +0800

    Forgot to save in vim i think

--- streams.py
+++ streams.py
@@ -254,6 +254,7 @@ class StreamsBot(irc.IRCClient):
     	if re.match('^.streams', msg):
     		#data = Streams()
 			if(data.lock == 0):
+				data.update_streams()
 				self.msg(channel, str(data.parse_for_channel(data.get_live_streams())))
 				data.lock = 1
 			#else:
